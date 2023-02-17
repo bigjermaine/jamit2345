e3e53f0db053395d4c84efd98cf29f0fd26e5941
daniel jermaine <danieljermaine2@gmail.com>
2023-02-17 18:48:59 +0100

    Initial Commit

---
+++ jamit/jamitApp.swift
@@ -0,0 +1,17 @@
+//
+//  jamitApp.swift
+//  jamit
+//
+//  Created by Apple on 17/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct jamitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
